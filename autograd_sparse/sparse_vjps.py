be511e5f91185e1df5b0173e45c5b48aeec73858
twhughes <tylerwhughes91@gmail.com>
2019-01-03 16:25:48 -0800

    passes two

--- autograd_sparse/sparse_vjps.py
+++ autograd_sparse/sparse_vjps.py
@@ -3,31 +3,44 @@ from .sparse_wrapper import dot
 import autograd.numpy as anp
 
 def _dot_vjp_0(ans, sparse, dense):
+  print('vjp0')
   if max(anp.ndim(sparse), anp.ndim(dense)) > 2:
     raise NotImplementedError("Current dot vjps only support ndim <= 2.")
 
   if anp.ndim(sparse) == 0:
+    print('  1st case')
     return lambda g: anp.sum(dense * g)
   if anp.ndim(sparse) == 1 and anp.ndim(dense) == 1:
+    print('  2nd case')
     return lambda g: g * dense
   if anp.ndim(sparse) == 2 and anp.ndim(dense) == 1:
+    print('  3rd case')
     return lambda g: g[:, None] * dense
   if anp.ndim(sparse) == 1 and anp.ndim(dense) == 2:
+    print('  4th case')
     return lambda g: anp.dot(dense, g)
-  return lambda g: anp.dot(g, dense.T)
+  print('  5th case')
+  return lambda g: sdot(dense.T, g)
 
 def _dot_vjp_1(ans, sparse, dense):
+  print('vjp1')
   if max(anp.ndim(sparse), anp.ndim(dense)) > 2:
     raise NotImplementedError("Current dot vjps only support ndim <= 2.")
 
   if anp.ndim(dense) == 0:
+    print('  1st case')
     return lambda g: anp.sum(sparse * g)
   if anp.ndim(sparse) == 1 and anp.ndim(dense) == 1:
+    print('  2nd case')
     return lambda g: g * sparse
   if anp.ndim(sparse) == 2 and anp.ndim(dense) == 1:
-    return lambda g: g @ sparse
+    print('  3rd case')
+    dense = dense[:, None]
+    return lambda g: dot(sparse.T, g)
   if anp.ndim(sparse) == 1 and anp.ndim(dense) == 2:
+    print('  4th case')
     return lambda g: sparse[:, None] * g
+  print('  5th case')
   return lambda g: dot(sparse.T, g)
 
 defvjp(dot, _dot_vjp_0, _dot_vjp_1)
--- tests/test_sparse.py
+++ tests/test_sparse.py
@@ -6,11 +6,12 @@ from autograd import grad
 import pdb
 from autograd.test_util import check_grads
 
+N = 5
 
 # ----- pytest fixture for sparse arrays ----- #
 @pytest.fixture
 def eye():
-    return sp.eye(5).tocsr()
+    return sp.eye(N).tocsr()
 
 
 # ----- tests for array creation ----- #
@@ -22,25 +23,59 @@ def test_sparse_coo_matrix():
     rows = np.array([1, 2, 3]).astype('float32')
     cols = np.array([1, 3, 4]).astype('float32')
     sparse = sp.coo_matrix(data, (rows, cols))
-    print(sparse.shape)
 
 
 # ----- tests for array multiplication ----- #
 @pytest.mark.sparse
 def test_sparse_dense_multiplication(eye):
     """This just has to not error out."""
-    dense = np.random.random(size=(5, 5))
+    dense = np.random.random(size=(N, N))
     sp.dot(eye, dense)
     sp.dot(dense, eye)
 
 
+# ----- tests for dot product ----- #
 
+
+# ----- vjp0 ----- #
+
+# # case 5
+# @pytest.mark.test
+# @pytest.mark.sp_sparse
+# def test_sparse_dot_0_5(eye):
+#     dense = np.random.random(size=(N, N))
+#     sparse = eye
+#     def fun(x):
+#         return sp.dot(x, dense)
+#     check_grads(fun)(sparse)
+
+# ----- vjp1 ----- #
+
+# case 3
 @pytest.mark.test
 @pytest.mark.sp_sparse
-def test_sparse_dot(eye):
-    dense = np.random.random(size=(1, 1))
+def test_sparse_dot_1_3(eye):
+    dense = np.random.random(size=(N, ))
     sparse = eye
     def fun(x):
-        return sp.dot(sparse, x)
+        return sp.dot(sparse, x)       
+    check_grads(fun)(dense)
 
-    check_grads(fun)(np.ones([5, 1]))
+# case 5
+@pytest.mark.test
+@pytest.mark.sp_sparse
+def test_sparse_dot_1_5(eye):
+    dense = np.random.random(size=(N, N))
+    sparse = eye
+    def fun(x):
+        return sp.dot(sparse, x)       
+    check_grads(fun)(dense)
+
+
+
+
+
+
+
+
+
